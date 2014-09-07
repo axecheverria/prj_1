8070a492090eb66f1262914025b235e913be2cea
Andres Echeverria <anecca@utexas.edu>
2014-09-06 19:18:03 -0500

    .

--- hello.py
+++ hello.py
@@ -4,9 +4,19 @@ from flask import render_template
 import os
 import subprocess
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def my_form():
     return render_template("my_form.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    multiply_text = text * 3
+   
+    return multiply_text
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port) # app.run()
