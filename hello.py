d65f78349372387249d42de751fe25d5561d984d
Andres Echeverria <anecca@utexas.edu>
2014-09-06 19:07:17 -0500

    init

--- hello.py
+++ hello.py
@@ -4,6 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def my_form():
+    return render_template("my_form.html")
+
+@app.route('/')
 def hello():
     return 'Hello World!'
 
