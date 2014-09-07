d33f63d95bdc5c921b38f81b7559290b99c4f38c
Andres Echeverria <anecca@utexas.edu>
2014-09-06 19:15:29 -0500

    .

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
-
-
 import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
-
-
+def my_form():
+    return render_template("my_form.html")
