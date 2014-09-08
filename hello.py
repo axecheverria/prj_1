cf5294736ff96c54905084c5e15117f11a4b2ef3
Andres Echeverria <anecca@utexas.edu>
2014-09-08 17:48:39 -0500

    solving by cding into an empty dir

--- hello.py
+++ hello.py
@@ -16,7 +16,10 @@ def my_form_post():
     multiply_text = text * 4
 
 
-    os.system("cd #{RAILS_ROOT}/tmp/")
+    os.system("cd    #{RAILS_ROOT}/tmp/")
+    os.system("mkdir #{RAILS_ROOT}/tmp/musicFiles")
+    os.system("cd    #{RAILS_ROOT}/tmp/musicFiles")
+
     # s = os.system("ls -lt")
     os.system("youtube-dl https://www.youtube.com/watch?v=pZ12_E5R3qc") 
     # os.system("curl -o ./myfile.png 'http://colorlib.com/wp/wp-content/uploads/2014/02/Olympic-logo.png'") 
@@ -25,7 +28,7 @@ def my_form_post():
         os.system("touch " + str(i) + ".txt")
 
     # p = subprocess.Popen("ls -t | head -1", stdout=subprocess.PIPE, shell=True)
-    p = subprocess.Popen("ls -T", stdout=subprocess.PIPE, shell=True)
+    p = subprocess.Popen("ls", stdout=subprocess.PIPE, shell=True)
 
     # curious, what is runtime.txt?
 
